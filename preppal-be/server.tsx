d3d418aeff45f0e8bfa3024daebab553b5850e4a
hwbit <83245042+hwbit@users.noreply.github.com>
2024-02-08 06:21:16 -0600

    setup for mongodb

---
+++ preppal-be/server.tsx
@@ -0,0 +1,11 @@
+const express = require('express');
+const connectToDB = require('./configs/db.tsx');
+const app = express();
+
+app.get('/', (req, res) => res.send('Hello world!'));
+
+const port = process.env.PORT || 9001;
+
+connectToDB.connectDB();
+
+app.listen(port, () => console.log(`Server running on port ${port}`));
