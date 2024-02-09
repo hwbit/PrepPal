6b7a9e7a00319ac5ad3c2b3a3ba4ec994e79bad5
hwbit <83245042+hwbit@users.noreply.github.com>
2024-02-09 09:32:32 -0600

    reorganized function calls, added export

--- preppal-be/server.ts
+++ preppal-be/server.ts
@@ -2,10 +2,12 @@ const express = require('express');
 const connectToDB = require('./configs/db.ts');
 const app = express();
 
-app.get('/', (req, res) => res.send('Hello world!'));
-
 const port = process.env.PORT || 9001;
 
 connectToDB.connectDB();
 
+app.get('/', (req, res) => res.send('Hello world!'));
+
 app.listen(port, () => console.log(`Server running on port ${port}`));
+
+module.exports = app;
