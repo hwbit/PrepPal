37eb54f9f5dc598371436f57cd05cf8fa4c66c23
Frieda <fbikangaroo46@gmail.com>
2024-02-10 22:55:39 -0600

    completed framework for individual recipe page

--- preppal-fe/src/pages/recipe.tsx
+++ preppal-fe/src/pages/recipe.tsx
@@ -1,20 +1,55 @@
-import { Container, Image, Stack, Card} from 'react-bootstrap';
+import { Container, Image, Stack, Row, Col, Card} from 'react-bootstrap';
+import {FaStar, FaRegStar, FaHeart} from 'react-icons/fa';
 const chickenfriedrice = require('../assets/chickenfriedrice.webp')
 
 export default function Recipe() {
     return (
-        <Container className="d-flex flex-column">
-            <html>
-                <h1>Chicken's Fried Rice</h1>
+        <Container className="d-flex ">
+        <html>
+            <h1>Chicken's Fried Rice</h1>
+            <Stack direction="horizontal">
+                <div className="p-2">
+                    {[...Array(4)].map(star => {
+                        return <FaStar/>
+                    })}
+                    <FaRegStar/> 4.2
+                </div>
+                <div className="p-2">
+                    43 Reviews
+                </div>
+                <div className="p-2">
+                    10 Photos
+                </div>
+            </Stack>
+            <div>No MSG required{"\n"}</div>
+            <Stack direction="horizontal">
+                <div className="p-2">
+                    Author: _
+                </div>
+                <div className="p-2">
+                    Date published: January 1, 2000
+                </div>
+            </Stack>
+            <Stack direction="horizontal" gap={4}>
                 <div>
-                    <Image src={chickenfriedrice} width={200}/>
+                    <FaStar/> Rate
                 </div>
-                <div className="d-flex flex-row justify-content">
-                    <div>Prep Time:</div>
-                    <div>Cook Time:</div>
-                    <div>Total Time:</div>
-                    <div>Servings:</div>
-                </div>
+                <div><FaHeart/> Favourite</div>
+            </Stack>
+            
+            <div>
+                <Image src={chickenfriedrice} width={200}/>
+            </div>
+            <Card>
+                <Row className="">
+                    <Col>Prep Time: 20 min</Col>
+                    <Col>Cook Time: 20 min</Col>
+                </Row>
+                <Row>
+                    <Col>Total Time: 40 min</Col>
+                    <Col>Servings: 1</Col>
+                </Row>
+            </Card>
                 <h1>Ingredients</h1>
                 <li>Chicken</li>
                 <li>Fried</li>
@@ -27,9 +62,23 @@ export default function Recipe() {
                     <li>Profit</li>
                 </ol>
                 <h1>Nutrition Facts</h1>
-
+                <Stack direction="horizontal" gap={3}>
+                    <div>Calories: __</div>
+                    <div>Fat: __</div>
+                    <div>Protein: __</div>
+                    <div>Carbohydrates: __</div>
+                </Stack>
                 <h1>Reviews</h1>
-                <Card></Card>
+                <Card>
+                    <div>User 1</div>
+                    <div>
+                        {[...Array(4)].map(star => {
+                            return <FaStar size={10}/>
+                        })}
+                        <FaRegStar size={10}/> 4.2
+                    </div>
+                    <div>no msg? this is a load of spicy chicken(trash)</div>
+                </Card>
             </html>
         </Container>
     )
