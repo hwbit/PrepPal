316c493c2de08612776674de42d0720f67384b03
Izan <cuetarai@myumanitoba.ca>
2024-02-12 13:37:45 -0600

    fixed card grid alignment and text overflow with ellipsis

--- preppal-fe/src/pages/home.tsx
+++ preppal-fe/src/pages/home.tsx
@@ -1,7 +1,7 @@
 // Import necessary React and TypeScript modules
 import React from 'react';
 import { Card, Col, Row } from 'react-bootstrap';
-import './home.css';
+import '../styles/global.css';
 
 // Define a type for recipe data
 type Recipe = {
@@ -28,22 +28,64 @@ const recipes: Recipe[] = [
     description: 'Quick, easy, and tasty macaroni and cheese dish. Fancy, designer mac and cheese often costs forty or fifty dollars to prepare when you have so many expensive cheeses, but they aren\'t always the best tasting. This simple recipe is cheap and tasty.',
     link: 'https://www.allrecipes.com/recipe/238691/simple-macaroni-and-cheese/',
   },
+  {
+    id: 3,
+    title: 'Grilled Cheese Sandwich',
+    imageUrl: 'https://images.media-allrecipes.com/userphotos/2206436.jpg',
+    description: 'Learn how to make a grilled cheese sandwich in a nonstick pan with buttered bread and American Cheddar for a classic hot sandwich.',
+    link: 'https://www.allrecipes.com/recipe/23891/grilled-cheese-sandwich/',
+  },
+  {
+    id: 4,
+    title: 'Simple Macaroni and Cheese',
+    imageUrl: 'https://www.the-girl-who-ate-everything.com/wp-content/uploads/2021/07/stovetop-mac-n-cheese-10.jpg',
+    description: 'Quick, easy, and tasty macaroni and cheese dish. Fancy, designer mac and cheese often costs forty or fifty dollars to prepare when you have so many expensive cheeses, but they aren\'t always the best tasting. This simple recipe is cheap and tasty.',
+    link: 'https://www.allrecipes.com/recipe/238691/simple-macaroni-and-cheese/',
+  },
+  {
+    id: 5,
+    title: 'Grilled Cheese Sandwich',
+    imageUrl: 'https://images.media-allrecipes.com/userphotos/2206436.jpg',
+    description: 'Learn how to make a grilled cheese sandwich in a nonstick pan with buttered bread and American Cheddar for a classic hot sandwich.',
+    link: 'https://www.allrecipes.com/recipe/23891/grilled-cheese-sandwich/',
+  },
+  {
+    id: 6,
+    title: 'Simple Macaroni and Cheese',
+    imageUrl: 'https://www.the-girl-who-ate-everything.com/wp-content/uploads/2021/07/stovetop-mac-n-cheese-10.jpg',
+    description: 'Quick, easy, and tasty macaroni and cheese dish. Fancy, designer mac and cheese often costs forty or fifty dollars to prepare when you have so many expensive cheeses, but they aren\'t always the best tasting. This simple recipe is cheap and tasty.',
+    link: 'https://www.allrecipes.com/recipe/238691/simple-macaroni-and-cheese/',
+  },
+  {
+    id: 7,
+    title: 'Grilled Cheese Sandwich',
+    imageUrl: 'https://images.media-allrecipes.com/userphotos/2206436.jpg',
+    description: 'Learn how to make a grilled cheese sandwich in a nonstick pan with buttered bread and American Cheddar for a classic hot sandwich.',
+    link: 'https://www.allrecipes.com/recipe/23891/grilled-cheese-sandwich/',
+  },
+  {
+    id: 8,
+    title: 'Simple Macaroni and Cheese',
+    imageUrl: 'https://www.the-girl-who-ate-everything.com/wp-content/uploads/2021/07/stovetop-mac-n-cheese-10.jpg',
+    description: 'Quick, easy, and tasty macaroni and cheese dish. Fancy, designer mac and cheese often costs forty or fifty dollars to prepare when you have so many expensive cheeses, but they aren\'t always the best tasting. This simple recipe is cheap and tasty.',
+    link: 'https://www.allrecipes.com/recipe/238691/simple-macaroni-and-cheese/',
+  },
 ];
 
 const ExplorePage: React.FC = () => {
   return (
     <div className="explore-page">
       <h1 style={{margin:'20px'}}>Explore</h1>
-      <Row className="g-4" xs={1} md={2}>
+      <Row xs="auto" md="auto" lg="auto">
         {recipes.map((recipe) => (
           <Col key={recipe.id}>
-            <Card className="recipe-card" style={{width: '18rem', height: '25rem', padding:'10px', margin:'10px'}}>
+            <Card className="recipe-card">
               <a href={recipe.link} target="_blank" rel="noopener noreferrer">
-                <Card.Img className='recipe-img' variant='top' src={recipe.imageUrl} alt={recipe.title} style={{ maxWidth: '18rem', maxHeight: '15rem', objectFit: 'cover', borderRadius: '5px' }}/>
+                <Card.Img className='recipe-img' variant='top' src={recipe.imageUrl} alt={recipe.title}/>
               </a>
               <Card.Body>
                 <Card.Title>{recipe.title}</Card.Title>
-                <Card.Text className='text-truncate' style={{height:'3em'}}>{recipe.description}</Card.Text>
+                <Card.Text className='recipe-text'>{recipe.description}</Card.Text>
               </Card.Body>
             </Card>
           </Col>
