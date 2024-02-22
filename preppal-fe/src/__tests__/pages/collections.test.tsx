2db0aebfb837c08ecf6b2c60c77bffbe18724462
lik5 <lik5@myumanitoba.ca>
2024-02-22 13:49:34 -0600

    fix test

--- preppal-fe/src/__tests__/pages/collections.test.tsx
+++ preppal-fe/src/__tests__/pages/collections.test.tsx
@@ -4,12 +4,10 @@ import Collections from "../../pages/collections";
 
 describe('Collections page tests', () => {
     test('Render standard component --> Buttons', async () => {
-        const result = '{"username":"testApiSandboxAccount","password":"lp12asr35Sa45@$"}';
-        await localStorage.setItem("token", JSON.stringify(result));
         render(<BrowserRouter><Collections /></BrowserRouter>);
 
-        const myRecipesBtn = screen.queryByTitle("My Recipes");
-        const favouritesBtn = screen.queryByTitle("Favourites");
+        const myRecipesBtn = screen.queryByText("My Recipes");
+        const favouritesBtn = screen.queryByText("Favourites");
 
         expect(myRecipesBtn).toBeTruthy();
         expect(favouritesBtn).toBeTruthy();
