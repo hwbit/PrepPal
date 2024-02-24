11c46432ba5e69b16a6c0f2481eadc749d863dde
Ryan Loeppky <wolfenergy@shaw.ca>
2024-02-23 21:35:59 -0600

    Set loggedIn bool to display profile/login nav links correctly.

--- preppal-fe/src/components/nav-bar/nav-bar.tsx
+++ preppal-fe/src/components/nav-bar/nav-bar.tsx
@@ -2,10 +2,38 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import { Form } from 'react-bootstrap';
 import './nav-bar.css'
-import { useState } from 'react';
+import React from 'react';
 
 const NavBar = () => {
-  const [loggedIn] = useState(false)
+  const [loggedIn, setLoggedIn] = React.useState(false)
+  const [user, setUser] = React.useState('')
+
+  React.useEffect(() => {
+    fillUserContent();
+    // eslint-disable-next-line
+  }, []);
+
+  const fillUserContent = async () => {
+    const token = localStorage.getItem("token");
+    try {
+      if (token) {
+        const req = {
+          method: "GET",
+          headers: {
+            "x-auth-token": token
+          }
+        };
+        const res = await fetch("http://localhost:9001/api/auth/", req).then(res => res.json());
+        setUser(res.username);
+        if (user != "")
+          setLoggedIn(true);
+      }
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+
   return (
     <Navbar expand="lg">
       <Navbar.Brand href="/">
@@ -20,7 +48,7 @@ const NavBar = () => {
       <Nav className="links">
         <Nav.Link className="nav-item" href="collections">Collections</Nav.Link>
         <Nav.Link className="nav-item" href="new-recipe">New Recipe</Nav.Link>
-        {loggedIn
+        {{loggedIn}
           ? (<Nav.Link className="nav-item" href="profile">Profile</Nav.Link>)
           : (<Nav.Link className="nav-item" href="login">Login</Nav.Link>)
         }
