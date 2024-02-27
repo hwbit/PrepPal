69495087c9f73f88101b54c74a5b46eaeae3bb89
Izan Cuetara Diez <cuetarai@myumanitoba.ca>
2024-02-27 11:53:17 -0600

    Updates

--- preppal-be/configs/secrets.ts
+++
@@ -1,5 +0,0 @@
-module.exports =
-{
-    mongoURI: "mongodb+srv://preppal:7vO9xvCVqlmm6wmF@cluster0.jxfk6bx.mongodb.net/?retryWrites=true&w=majority",
-    jwtSecret: "PrepPalSecretToken@COMP4350"
-}
