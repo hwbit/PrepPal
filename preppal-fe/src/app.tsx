327f0252d828216ca9d2caa645511f47964d3e00
Izan Cuetara Diez <cuetarai@myumanitoba.ca>
2024-02-27 17:27:11 -0600

    Added PrepPal basename

--- preppal-fe/src/app.tsx
+++ preppal-fe/src/app.tsx
@@ -13,18 +13,18 @@ import Signup from './pages/signup';
 
 function App() {
   return (
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <BrowserRouter basename="/PrepPal">
       <div className="app">
         <NavBar />
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="login" element={<LoginPage />} />
-          <Route path="signup" element={<Signup />} />
-          <Route path="profile" element={<Profile />} />
-          <Route path="edit-profile" element={<EditProfile />} />
-          <Route path="new-recipe" element={<NewRecipe />} />
-          <Route path="collections" element={<Collections />} />
-          <Route path="search/:q" element={<Search />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/edit-profile" element={<EditProfile />} />
+          <Route path="/new-recipe" element={<NewRecipe />} />
+          <Route path="/collections" element={<Collections />} />
+          <Route path="/search/:q" element={<Search />} />
         </Routes>
       </div>
     </BrowserRouter>
