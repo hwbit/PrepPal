32c0b9b2c63c5156fc97fa6d229f2a33994c3768
lik5 <lik5@myumanitoba.ca>
2024-03-02 12:11:33 -0600

    front-end api calls for save recipe

--- preppal-fe/src/components/fav-button/fav-button.tsx
+++ preppal-fe/src/components/fav-button/fav-button.tsx
@@ -1,50 +1,67 @@
 import React from "react";
 
 const FavouriteButton = (recipe: any) => {
-    const [favourited, setFavourite] = React.useState<boolean>(false);
-    const recipeId = recipe.id
-    React.useEffect(() => {
-        saveRecipe();
-    }, [favourited]);
+    const [favourite, setFavourite] = React.useState<boolean>(false);
 
-
-    async function saveRecipe() {
-        const token = sessionStorage.getItem('token');
+    async function saveRecipe(saveRecipeId: string, save: boolean) {
+        const token = sessionStorage.getItem("token");
         try {
             if (token) {
-                const req = {
-                    method: "POST",
+                if (save) {
+                    const req = {
+                        method: "POST",
 
-                    headers: {
-                        'Content-Type': 'application/json',
-                        "x-auth-token": token
-                    },
+                        headers: {
+                            "Content-Type": "application/json",
+                            "x-auth-token": token,
+                        },
 
-                    body: JSON.stringify({
-                        "recipeId": recipeId
-                    })
-                };
+                        body: JSON.stringify({ recipeId: saveRecipeId }),
+                    };
+                    const res = await fetch("http://localhost:9001/api/user/saveRecipe", req).then((res) => res.json());
+                }
+                else {
+                    const req = {
+                        method: "POST",
 
-                const res = await fetch("http://localhost:9001/api/user/favRecipe", req).then(res => res.json());
+                        headers: {
+                            "Content-Type": "application/json",
+                            "x-auth-token": token,
+                        },
 
-                setFavourite(res.saved);
+                        body: JSON.stringify({ recipeId: saveRecipeId }),
+                    };
+                    const res = await fetch("http://localhost:9001/api/user/unsaveRecipe", req).then((res) => res.json());
+                }
             }
-
-        } catch (err) {
+        }
+        catch (err) {
             console.log(err);
         }
-
     }
 
-    function handleClick(event: any): void {
-        setFavourite(!favourited);
+    function handleClick(): void {
+        setFavourite(!favourite);
+        saveRecipe(recipe.id, favourite);
     }
 
     return (
-        favourited 
-        ? <img onClick={handleClick} src={require("../../assets/filled-heart.png")} width={40} height={40} alt='fav'></img>
-        : <img onClick={handleClick} src={require("../../assets/unfilled-heart.png")} width={40} height={40} alt='fav'></img>
-    )
-}
+        favourite
+            ? (<img
+                onClick={handleClick}
+                src={require("../../assets/filled-heart.png")}
+                width={40}
+                height={40}
+                alt={"fav"}>
+            </img>)
+            : (<img
+                onClick={handleClick}
+                src={require("../../assets/unfilled-heart.png")}
+                width={40}
+                height={40}
+                alt={"fav"}>
+            </img>)
+    );
+};
 
 export default FavouriteButton;
