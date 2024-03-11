b6cd3a815e0e9eb7628eaaad652d8db36c0d4a81
lik5 <lik5@myumanitoba.ca>
2024-03-11 13:20:20 -0500

    wrap unit tests

--- preppal-fe/src/__tests__/pages/edit-profile.test.tsx
+++ preppal-fe/src/__tests__/pages/edit-profile.test.tsx
@@ -3,22 +3,25 @@ import EditProfile from "../../pages/edit-profile";
 import { BrowserRouter } from 'react-router-dom';
 
 describe('EditProfile component tests', () => {
-    test('Render standard component --> Buttons', () => {
-        render(<BrowserRouter><EditProfile /></BrowserRouter>);
 
-        const submitUpdate = screen.queryByTitle("SubmitUpdate");
-        expect(submitUpdate).toBeTruthy();
+    describe("Unit tests", () => {
+        test('Render standard component --> Buttons', () => {
+            render(<BrowserRouter><EditProfile /></BrowserRouter>);
+
+            const submitUpdate = screen.queryByTitle("SubmitUpdate");
+            expect(submitUpdate).toBeTruthy();
+        });
+
+        test('Render standard component --> Input fields', () => {
+            render(<BrowserRouter><EditProfile /></BrowserRouter>);
+
+            const oldPassword = screen.queryByTitle("inputOldPassword");
+            expect(oldPassword).toBeTruthy();
+            const newPassword = screen.queryByTitle("inputNewPassword");
+            expect(newPassword).toBeTruthy();
+            const bio = screen.queryByTitle("inputBio");
+            expect(bio).toBeTruthy();
+        });
     });
-
-    test('Render standard component --> Input fields', () => {
-        render(<BrowserRouter><EditProfile /></BrowserRouter>);
-
-        const oldPassword = screen.queryByTitle("inputOldPassword");
-        expect(oldPassword).toBeTruthy();
-        const newPassword = screen.queryByTitle("inputNewPassword");
-        expect(newPassword).toBeTruthy();
-        const bio = screen.queryByTitle("inputBio");
-        expect(bio).toBeTruthy();
-    });
-})
+});
 
