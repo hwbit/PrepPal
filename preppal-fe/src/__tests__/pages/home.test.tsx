7d81084e0eff7ca2ff5339362c160c743cad7936
lik5 <lik5@myumanitoba.ca>
2024-03-11 11:53:12 -0500

    home page tests

--- preppal-fe/src/__tests__/pages/home.test.tsx
+++ preppal-fe/src/__tests__/pages/home.test.tsx
@@ -1,20 +1,28 @@
+/* eslint-disable testing-library/no-wait-for-empty-callback */
 /* eslint-disable testing-library/no-unnecessary-act */
-import { render, screen } from "@testing-library/react";
+import { render, screen, waitFor } from "@testing-library/react";
 import { BrowserRouter } from 'react-router-dom';
 import Home from "../../pages/home";
+import { mockFetch } from "../mock-fetch";
 
 describe('Home component tests', () => {
-    // const testRecipe = {
-    //     author: "random author",
-    //     title: "recipe title",
-    //     titleUrl: "test-recipe-1234",
-    //     ingredients: ["1"],
-    //     instructions: ["1"],
-    //     servingSize: 1,
-    //     prepTime: 1,
-    //     cookingTime: 1,
-    //     isPublic: false,
-    // };
+    const testRecipe = {
+        _id: "recipe-id",
+        author: "random author",
+        description: "random desc",
+        title: "recipe title",
+        titleUrl: "test-recipe-1234",
+        ingredients: ["1"],
+        instructions: ["1"],
+        servingSize: 1,
+        prepTime: 1,
+        cookingTime: 1,
+        isPublic: false,
+    };
+
+    afterEach(() => {
+        sessionStorage.removeItem('token');
+    });
 
     describe("Unit tests", () => {
         test('Render standard component --> Title', () => {
@@ -25,34 +33,54 @@ describe('Home component tests', () => {
         });
     });
 
-    // describe("Integration tests", () => {
-    //     test('Clicking on recipe card navigates to selected recipe', async () => {
-    //         await render(<BrowserRouter><Home /></BrowserRouter>);
-    //         window.fetch = mockFetch({ "recipes": [testRecipe] });
-    //         await waitFor(() => {
-    //             expect(screen.getByText(testRecipe.title)).toBeTruthy();
-    //         });
+    describe("Integration tests", () => {
+        test('Recipe catalog populates home page with recipes', async () => {
+            window.fetch = mockFetch([testRecipe]);
+            await render(<BrowserRouter><Home /></BrowserRouter>);
 
-    //         await act(() => fireEvent.click(screen.getByText(testRecipe.title)));
+            await waitFor(() => { });
 
-    //         // await act(() => render(<BrowserRouter><Recipe /></BrowserRouter>));
+            const recipeCard = screen.queryByText(testRecipe.title)
+            expect(recipeCard).toBeTruthy();
 
-    //         const recipeTitle = screen.getByText(testRecipe.title);
-    //         const recipeIngredients = screen.getByText(testRecipe.ingredients[0]);
-    //         const recipeInstructions = screen.getByText(testRecipe.instructions[0]);
-    //         const recipeAuthor = screen.getByText(testRecipe.author);
-    //         const recipeServingSize = screen.getByText(testRecipe.servingSize);
-    //         const recipeCookingTime = screen.getByText(testRecipe.cookingTime);
-    //         const recipePrepTime = screen.getByText(testRecipe.prepTime);
 
-    //         expect(recipeTitle).toBeTruthy();
-    //         expect(recipeInstructions).toBeTruthy();
-    //         expect(recipeIngredients).toBeTruthy();
-    //         expect(recipeCookingTime).toBeTruthy();
-    //         expect(recipePrepTime).toBeTruthy();
-    //         expect(recipeAuthor).toBeTruthy();
-    //         expect(recipeServingSize).toBeTruthy();
-    //     });
-    // });
+            const recipeTitle = screen.getByText(testRecipe.title);
+            const recipeAuthor = screen.getByText(testRecipe.author);
+            const recipeDesc = screen.getByText(testRecipe.description);
+
+            expect(recipeTitle).toBeTruthy();
+            expect(recipeAuthor).toBeTruthy();
+            expect(recipeDesc).toBeTruthy();
+        });
+
+        //TODO: move to recipe card tests
+        // test('Clicking on recipe card navigates to selected recipe', async () => {
+        //     window.fetch = mockFetch([testRecipe]);
+        //     await render(<BrowserRouter><Home /></BrowserRouter>);
+
+        //     await waitFor(() => { });
+
+        //     const link = screen.getByTitle("recipe-link");
+        //     await act(() => fireEvent.click(link));
+
+        //     await waitFor(() => { });
+
+        //     const recipeTitle = screen.getByText(testRecipe.title);
+        //     const recipeIngredients = screen.getByText(testRecipe.ingredients[0]);
+        //     const recipeInstructions = screen.getByText(testRecipe.instructions[0]);
+        //     const recipeAuthor = screen.getByText(testRecipe.author);
+        //     const recipeServingSize = screen.getByText(testRecipe.servingSize);
+        //     const recipeCookingTime = screen.getByText(testRecipe.cookingTime);
+        //     const recipePrepTime = screen.getByText(testRecipe.prepTime);
+
+        //     expect(recipeTitle).toBeTruthy();
+        //     expect(recipeInstructions).toBeTruthy();
+        //     expect(recipeIngredients).toBeTruthy();
+        //     expect(recipeCookingTime).toBeTruthy();
+        //     expect(recipePrepTime).toBeTruthy();
+        //     expect(recipeAuthor).toBeTruthy();
+        //     expect(recipeServingSize).toBeTruthy();
+        // });
+    });
 })
 
--- preppal-fe/src/components/recipe-card/recipe-card.tsx
+++ preppal-fe/src/components/recipe-card/recipe-card.tsx
@@ -7,8 +7,8 @@ function RecipeCard(recipe: any): JSX.Element {
   const recipeImage = require(`../../assets/${recipe.image}`);
   return (
     <Card className="recipe-card">
-      <Link to={"/recipe/"+recipe._id} >
-        <Card.Img className='recipe-img' variant='top' src={recipeImage} alt={recipe.title} />
+      <Link to={"/recipe/" + recipe._id} title="recipe-link">
+        <Card.Img className='recipe-img' variant='top' src={recipeImage} alt={recipe.title} title="recipe-img" />
       </Link>
       <Card.Body>
         <Card.Title className='recipe-title'>{recipe.title}</Card.Title>
