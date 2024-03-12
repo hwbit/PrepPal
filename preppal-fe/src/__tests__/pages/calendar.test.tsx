d113cd03bf37ae94dc1cbd6a3080b290be513ff0
lik5 <lik5@myumanitoba.ca>
2024-03-12 15:33:44 -0500

    clean up calendar tests

--- preppal-fe/src/__tests__/pages/calendar.test.tsx
+++ preppal-fe/src/__tests__/pages/calendar.test.tsx
@@ -3,6 +3,10 @@ import CalendarPage from "../../pages/recipe-calendar";
 import { BrowserRouter } from 'react-router-dom';
 
 describe('Calendar Component tests', () => {
+    beforeEach(() => {
+        jest.spyOn(console, 'error').mockImplementation(jest.fn());
+    });
+
     test('Render standard component --> Input fields', () => {
         render(<BrowserRouter><CalendarPage /></BrowserRouter>);
 
