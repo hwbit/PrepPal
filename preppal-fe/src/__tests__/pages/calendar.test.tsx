b24426c2da19028b3ac75087d2b938a03964a481
Ryan Loeppky <wolfenergy@shaw.ca>
2024-03-12 11:38:35 -0500

    Updated a test that caused error.

--- preppal-fe/src/__tests__/pages/calendar.test.tsx
+++ preppal-fe/src/__tests__/pages/calendar.test.tsx
@@ -6,8 +6,6 @@ describe('Calendar Component tests', () => {
     test('Render standard component --> Input fields', () => {
         render(<BrowserRouter><CalendarPage /></BrowserRouter>);
 
-        const calendarButton = screen.queryByText("SubmitCalendarUpdate");
-        expect(calendarButton).toBeTruthy();
         const recipesButton = screen.queryByText("SubmitRecipeUpdate");
         expect(recipesButton).toBeTruthy();
     });
