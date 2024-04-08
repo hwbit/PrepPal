61cfb861d12c153687a5bf685d3d677f29bf0041
hwbit <83245042+hwbit@users.noreply.github.com>
2024-04-08 11:07:01 -0500

    more typo fix

--- preppal-be/auth/authorization.ts
+++ preppal-be/auth/authorization.ts
@@ -1,6 +1,6 @@
 const jwtAuth = require("jsonwebtoken");
 require("dotenv").config();
-const jwtSecret = process.env.JWT_SECRETS;
+const jwtSecret = process.env.JWT_SECRET;
 
 
 module.exports = function(req, res, next) {
